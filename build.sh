b795cc4b23a4860fdbb73a6b9bea817e07a9ddd4
genx7up <genx7up@noreply.users.github.com>
2020-07-02 21:18:12 +0000

    Add Github Action

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 set -xe
 
-docker login ${APP_REGISTRY_ENDPOINT} --username ${APP_REGISTRY_USERNAME} --password ${APP_REGISTRY_PASSWORD} 2>/dev/null
-docker build --rm=true --pull=true -t ${APP_REGISTRY_REPO} -f Dockerfile .
-docker push ${APP_REGISTRY_REPO}
+docker login ${REGISTRY_ENDPOINT} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD} 2>/dev/null
+docker build --rm=true --pull=true -t ${REGISTRY_REPO} -f Dockerfile .
+docker push ${REGISTRY_REPO}
+{APP_REGISTRY_REPO}
