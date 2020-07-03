0066bedfd47f5b739d1abbb7cc4dd8e68800e9ad
genx7up <genx7up@noreply.users.github.com>
2020-07-03 05:04:45 +0000

    Add CircleCI Action

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ set -xe
 
 docker login ${REGISTRY_ENDPOINT} --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD} 2>/dev/null
 docker build --rm=true --pull=true -t ${REGISTRY_REPO} -f Dockerfile .
-docker push ${REGISTRY_REPO}
+docker push ${REGISTRY_REPO} ${REGISTRY_REPO}:actions
+docker push ${REGISTRY_REPO}:actions
