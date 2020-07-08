c4356e5a67ee60e97a9d4b3bd3f80cdf1796d7a8
genx7up <genx7up@noreply.users.github.com>
2020-07-08 00:11:42 +0000

    Add dispatch trigger

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ docker login ${REGISTRY_ENDPOINT} --username ${REGISTRY_USERNAME} --password ${R
 docker build --rm=true --pull=true -t ${REGISTRY_REPO} -f Dockerfile .
 docker tag ${REGISTRY_REPO} ${REGISTRY_REPO}:actions
 docker push ${REGISTRY_REPO}:actions
-
